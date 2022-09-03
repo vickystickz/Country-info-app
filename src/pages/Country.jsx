87bafcb7f5ab9aede0d062e1c14383dd529aec82
vickystickz <kademvicky@gmail.com>
2022-09-03 11:38:27 +0100

    edited link

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -60,14 +60,14 @@ const Country = () => {
                             key={id}
                             initial={{opacity:0 ,translateX:-50, translateY:-50}} 
                             animate={{opacity:1 , translateX:0 , translateY:0}} 
-                            transition={{duration: 0.1}}
+                            transition={{duration: 0.01}}
                             className="w-80 h rounded border-1 border-white text-left">
                                 <img className='w-full h-48 object-cover rounded ' src={country.flags.png} alt={country.name.common} />
                                 <h1 className='flex text-2xl font-bold mt-2'>{country.name.common}</h1>
                                 <div className='flex'><h3 className='text-lg font-medium leading-normal mr-3'>Continent:</h3> <p className="leading-7 inline">{country.continents}</p></div>
                                 <div className='flex'><h3 className='text-lg font-medium leading-normal mr-3'>Capital:</h3> <p className="leading-7 inline">{country.capital}</p></div>
                                 <div className='flex'><h3 className='text-lg font-medium leading-normal mr-3'>Total Population:</h3> <p >{country.population}</p></div>
-                                <a className=" pb-2  text-azure-blue transition-all delay-300 ease-in hover:border-b-2 hover:border-azure-blue"href={country.maps.openStreetMaps} >Map view</a>
+                                <a className=" pb-2  text-azure-blue transition-all delay-300 ease-in hover:border-b-2 hover:border-azure-blue"href={country.maps.openStreetMaps} target="_blank" >Map view</a>
                             
                         </motion.div> ))}
             </div>  }
