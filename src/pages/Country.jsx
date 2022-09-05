4d2b41798c61e1f9475a7958b9757c18c8e25277
vickystickz <kademvicky@gmail.com>
2022-09-05 10:04:17 +0100

    added aos library

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
+import AOS from 'aos';
 import { Oval } from  'react-loader-spinner'
 import axios from 'axios';
-import { motion } from 'framer-motion';
 const Country = () => {
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+      }, []);
+
     const baseURL = "https://restcountries.com/v3.1/all";
 
     // handles states for data 
@@ -54,13 +60,9 @@ const Country = () => {
             />
             </div> : 
 
-            <div className='grid  justify-center lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-12 my-10'>
+            <div data-aos="fade-up" className='grid  justify-center lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-12 my-10'>
                 {countries.map((country,id) => (
-                        <motion.div 
-                            key={id}
-                            initial={{opacity:0 ,translateX:-50, translateY:-50}} 
-                            animate={{opacity:1 , translateX:0 , translateY:0}} 
-                            transition={{duration: 0.01}}
+                        <div 
                             className="w-80 h rounded border-1 border-white text-left">
                                 <img className='w-full h-48 object-cover rounded ' src={country.flags.png} alt={country.name.common} />
                                 <h1 className='flex text-2xl font-bold mt-2'>{country.name.common}</h1>
@@ -69,7 +71,7 @@ const Country = () => {
                                 <div className='flex'><h3 className='text-lg font-medium leading-normal mr-3'>Total Population:</h3> <p >{country.population}</p></div>
                                 <a className=" pb-2  text-azure-blue transition-all delay-300 ease-in hover:border-b-2 hover:border-azure-blue"href={country.maps.googleMaps} target="_blank" >Map view</a>
                             
-                        </motion.div> ))}
+                        </div> ))}
             </div>  }
     </>
   )
