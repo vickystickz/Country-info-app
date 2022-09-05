aecf510186f90ad38a2a7d93fa69bd9932ba322c
vickystickz <kademvicky@gmail.com>
2022-09-05 10:40:43 +0100

    added Aos CSS

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { Oval } from  'react-loader-spinner'
 import axios from 'axios';
 const Country = () => {
 
     useEffect(() => {
-        AOS.init();
+        AOS.init({
+            duration: 500,
+        });
         AOS.refresh();
       }, []);
 
@@ -60,16 +63,16 @@ const Country = () => {
             />
             </div> : 
 
-            <div data-aos="fade-up" className='grid  justify-center lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-12 my-10'>
+            <div className='grid  justify-center lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-12 my-10'>
                 {countries.map((country,id) => (
-                        <div 
+                        <div data-aos="fade-up"
                             className="w-80 h rounded border-1 border-white text-left">
                                 <img className='w-full h-48 object-cover rounded ' src={country.flags.png} alt={country.name.common} />
                                 <h1 className='flex text-2xl font-bold mt-2'>{country.name.common}</h1>
                                 <div className='flex'><h3 className='text-lg font-medium leading-normal mr-3'>Continent:</h3> <p className="leading-7 inline">{country.continents}</p></div>
                                 <div className='flex'><h3 className='text-lg font-medium leading-normal mr-3'>Capital:</h3> <p className="leading-7 inline">{country.capital}</p></div>
                                 <div className='flex'><h3 className='text-lg font-medium leading-normal mr-3'>Total Population:</h3> <p >{country.population}</p></div>
-                                <a className=" pb-2  text-azure-blue transition-all delay-300 ease-in hover:border-b-2 hover:border-azure-blue"href={country.maps.googleMaps} target="_blank" >Map view</a>
+                                <a className=" pb-2  text-azure-blue transition-all delay-300 ease-in hover:border-b-2 hover:border-azure-blue"href={country.maps.googleMaps} target="_blank" rel="noreferrer">Map view</a>
                             
                         </div> ))}
             </div>  }
